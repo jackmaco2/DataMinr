e3768e325aa8bba50e2d83e5361eedbada5dbf4a
Jack <jackcountryman@gmail.com>
2018-11-09 12:27:01 -0600

    Update DataMinrv0.1.py

--- DataMinrv0.1.py
+++ DataMinrv0.1.py
@@ -5,7 +5,7 @@ Reading:
 Article on Beautiful soup (web scraping package for python) : https://medium.freecodecamp.org/how-to-scrape-websites-with-python-and-beautifulsoup-5946935d93fe
 
 MetaJack Tools
-By Jack Countryman
+By Jack Countryman & Nate Wilwerding
 '''
 
 def useage() :
